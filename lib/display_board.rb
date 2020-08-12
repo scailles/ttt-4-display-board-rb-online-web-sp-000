00a414c8cca8b0e8fa1b699577f93b25ef53b2ee
Shaina C <scailles23@gmail.com>
2020-08-12 03:39:38 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -3,18 +3,14 @@
 
 
 
-def board
-  puts "   |   |   
------------
-   |   |   
------------
-   |   |   "
- end
- 
- 
- def display_board(board)
-   
-   
- end
- 
+def display_board(board)
+separator = "|"
+lines = "-----------"
+
+puts " #{board[0]} #{separator} #{board[1]} #{separator} #{board[2]} "
+puts "#{lines}"
+puts " #{board[3]} #{separator} #{board[4]} #{separator} #{board[5]} "
+puts "#{lines}"
+puts " #{board[6]} #{separator} #{board[7]} #{separator} #{board[8]} "
+end
   
